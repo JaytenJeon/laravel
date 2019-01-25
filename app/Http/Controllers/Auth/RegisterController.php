a9298dbfa1afc86358546eef00ea3d90037471ba
JaytenJeon <jayten0821@gmail.com>
2019-01-25 14:58:48 +0000

    fix some bug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'login_id' => $data['name'],
+            'login_id' => $data['login_id'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
