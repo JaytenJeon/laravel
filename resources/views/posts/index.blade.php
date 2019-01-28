1e6f6bfd40264181dc38ffd8b11ab97c81801a90
JaytenJeon <jayten0821@gmail.com>
2019-01-28 13:28:40 +0000

    Fix Posts table column width

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,12 +6,12 @@
                 <div class="card">
                     <div class="card-header">프로그래밍 갤러리</div>
                     <div class="card-body p-0 pr-3 pl-3">
-                        <table class="table">
+                        <table class="table table-hover">
                             <thead  >
                             <tr>
-                                <th>번호</th>
-                                <th class="text-center">제목</th>
-                                <th>작성자</th>
+                                <th style="width:10%">번호</th>
+                                <th style="width:70%" class="text-center">제목</th>
+                                <th style="width:20%" class="">작성자</th>
                             </tr>
                             </thead>
                             <tbody>
