12d0b6a5f6fd43636bd9f6b665d013c919e316f8
JaytenJeon <jayten0821@gmail.com>
2019-01-28 13:01:35 +0000

    Replace Field Name: Name to Nickname

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'login_id', 'name', 'email', 'password'
+        'login_id', 'nickname', 'email', 'password'
     ];
 
     /**
